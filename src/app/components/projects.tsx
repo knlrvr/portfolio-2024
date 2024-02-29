f8f5ae0f3df0449b2ac5527f79f1c063cf000cad
Kane <kanelariviere@outlook.com>
2024-02-28 20:49:19 -0500

    proj links

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -12,32 +12,32 @@ export default function Projects() {
                 <ProjectCard
                     url='https://github.com/knlrvr/ttrpcompanion'
                     title='TTRPCompanion'
-                    description='A gaming utility app that allows users to track full campaign stats in D&D & other TTRPGs.'
+                    description='A gaming utility app that allows users to track full campaign stats in D&D & other TTRPGs. Built with T3.'
                 />
                 <ProjectCard
-                    url='https://github.com/knlrvr/ttrpcompanion'
+                    url='https://theskyisfake.org'
                     title='The Sky Is Fake'
-                    description='An image sharing platform dedicated to pictures of the sky that just don&apos;t seem real.'
+                    description='An image sharing platform dedicated to pictures of the sky that just don&apos;t seem real. Built with Next.js & Convex.'
                 />
                 <ProjectCard
-                    url='https://github.com/knlrvr/ttrpcompanion'
+                    url='https://github.com/knlrvr/theviewfrominside'
                     title='The View From Inside'
                     description='A blog built with Next.js & Sanity.io for content management. '
                 />
                 <ProjectCard
-                    url='https://github.com/knlrvr/ttrpcompanion'
+                    url='https://github.com/knlrvr/keepup'
                     title='KeepUp'
-                    description='A social media platform that wasn&apos;t supposed to be a twitter clone but is.'
+                    description='A social media platform that wasn&apos;t supposed to be a twitter clone but is. Built with T3.'
                 />
                 <ProjectCard
                     url='https://github.com/knlrvr/ttrpcompanion'
                     title='Abstract'
-                    description='An ecommerce store that offers conceptual products related to consumerism and materialism.'
+                    description='An ecommerce store that offers conceptual products related to consumerism and materialism. Built with Next.kjs & MongoDB.'
                 />
                 <ProjectCard
                     url='https://github.com/knlrvr/ttrpcompanion'
                     title='knlrvr.dev'
-                    description='My personal portfolio.'
+                    description='My personal portfolio (where you are now)! Built with Next.js & Convex.'
                 />
             </div>
         </div>
