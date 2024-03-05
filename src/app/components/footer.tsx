1ff51610085326c209271c5714149556042eb349
Kane <kanelariviere@outlook.com>
2024-03-04 21:38:07 -0500

    acc updates

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -27,48 +27,48 @@ export default function Footer() {
                     <div className="flex flex-col space-y-2">
                         <div className="flex items-center gap-1">
                             <FiSmile />
-                            <p>Built by <Link href="https://github.com/knlrvr" target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">Kane Lariviere</Link>.</p>
+                            <p>Built by <Link href="https://github.com/knlrvr" target="_blank" aria-label='GitHub' className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">Kane Lariviere</Link>.</p>
                         </div>
                         <div className="flex items-center gap-1">
                             <FiCode /> 
                             <p>Built with&nbsp;
-                                <Link href="https://nextjs.org/" target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">
+                                <Link href="https://nextjs.org/" target="_blank" aria-label='Next.js' className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">
                                     Next.js
                                 </Link>,&nbsp;
-                                <Link href="https://tailwindcss.com/" target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">
+                                <Link href="https://tailwindcss.com/" target="_blank" aria-label='Tailwind' className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">
                                     Tailwind
                                 </Link>, &&nbsp;
-                                <Link href="https://www.convex.dev/" target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">
+                                <Link href="https://www.convex.dev/" target="_blank" aria-label='Convex' className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">
                                     Convex
                                 </Link>.
                             </p>
                         </div>
                         <div className="flex items-center gap-1">
                             <FiGithub />
-                            <p>Source code available on <Link href="https://github.com/knlrvr/kanelariviere-new" target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">GitHub</Link>.</p>
+                            <p>Source code available on <Link href="https://github.com/knlrvr/kanelariviere-new" target="_blank" aria-label='Source Code on GitHub' className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">GitHub</Link>.</p>
                         </div>
                         <div className="flex items-center gap-1">
                             <FiTriangle />
-                            <p>Hosted on <Link href="https://vercel.com" target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">Vercel</Link>.</p>
+                            <p>Hosted on <Link href="https://vercel.com" target="_blank" aria-label='Vercel Hosting' className="underline underline-offset-4 hover:text-neutral-200 transition-colors duration-200">Vercel</Link>.</p>
                         </div>
                     </div>
                     <div className="flex items-center gap-2 text-lg sm:text-sm">
-                        <Link href="mailto:hello@knlrvr.com" target="_blank" className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
+                        <Link href="mailto:hello@knlrvr.com" target="_blank" aria-label='Email' className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
                             <FaEnvelope />
                         </Link>
-                        <Link href="https://github.com/knlrvr" target="_blank" className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
+                        <Link href="https://github.com/knlrvr" target="_blank" aria-label='knlrvr on GitHub' className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
                             <FiGithub />
                         </Link>
-                        <Link href="https://www.linkedin.com/in/kane-lariviere" target="_blank" className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
+                        <Link href="https://www.linkedin.com/in/kane-lariviere" target="_blank" aria-label='LinkedIn' className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
                             <FiLinkedin />
                         </Link>
-                        <Link href="https://twitter.com/knlrvr" target="_blank" className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
+                        <Link href="https://twitter.com/knlrvr" target="_blank" aria-label='Twitter' className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
                             <FaXTwitter />
                         </Link>
-                        <Link href="https://www.threads.net/@kanelarivieresucks" target="_blank" className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
+                        <Link href="https://www.threads.net/@kanelarivieresucks" target="_blank" aria-label='Threads' className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
                             <FaThreads />
                         </Link>
-                        <Link href="https://www.twitch.tv/knlrvr" target="_blank" className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
+                        <Link href="https://www.twitch.tv/knlrvr" target="_blank" aria-label='Twitch' className="border border-neutral-500 p-1 rounded-sm hover:text-neutral-200 hover:border-neutral-200 transition-colors duration-200">
                             <FaTwitch />
                         </Link>
                     </div>
