796e9897459c4153e4003aa0eb01ce4a4f02556b
Kane <kanelariviere@outlook.com>
2024-03-27 15:45:40 -0400

    ultra secret tech page

---
+++ src/app/tech/page.tsx
@@ -0,0 +1,88 @@
+
+
+export default function Tech() {
+    return (
+        <div>
+            <span className="text-neutral-300 text-sm font-light tracking-wide leading-6 mt-2">
+                Since you found my <em className="text-blue-200">ultra secret</em> page, here&apos;s a list of the tech I use.
+            </span>
+
+            <div className="mt-8 flex flex-col space-y-8">
+
+                <div>
+                    <span className="font-medium tracking-wide">Office &mdash;</span>
+                    <ul className="text-sm list-disc ml-4 mt-2 text-neutral-500">
+                        <li>M1 Mac Mini (2020)</li>
+                        <li>M1 MacBook Air (2020)</li>
+                        <li>Apple Magic Keyboard</li>
+                        <li>Apple Magic Mouse</li>
+                        <li>27&quot; Samsung Curved Monitor</li>
+                        <li>24&quot; Dell Monitor</li>
+                        <li>Secretlab TITAN Evo Chair</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <span className="font-medium tracking-wide">Coding &mdash;</span>
+                    <ul className="text-sm list-disc ml-4 mt-2 text-neutral-500">
+                        <li><strong>Editor</strong>: VSCode </li>
+                        <li><strong>Theme</strong>: Vitesse Black</li>
+                        <li><strong>Terminal</strong>: Warp</li>
+                        <li><strong>Exploring</strong>: Neovim & Zed</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <span className="font-medium tracking-wide">Organization &mdash;</span>
+                    <ul className="text-sm list-disc ml-4 mt-2 text-neutral-500">
+                        <li>Obsidian</li>
+                        <li>Excalidraw</li>
+                        <li>Notion <em>(sometimes)</em></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <span className="font-medium tracking-wide">Gaming &mdash;</span>
+                    <ul className="text-sm list-disc ml-4 mt-2 text-neutral-500">
+                        <li>Windows 10</li>
+                        <li>Intel Core i5 9400F</li>
+                        <li>MSI MAG B365M MORTAR</li>
+                        <li>16GB T-Force Delta DDR4</li>
+                        <li>NVIDIA GeForce GTX 1660 SUPER</li>
+                        <li>Corsair K55 RGB Pro Keyboard</li>
+                        <li>Corsair M65 RGB ULTRA Mouse</li>
+                        <li>Corsair VOID RGB ELITE Headset</li>
+                        <li>Streamlabs</li>
+                        <li>Nintendo Switch</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <span className="font-medium tracking-wide">Audio / Video &mdash;</span>
+                    <ul className="text-sm list-disc ml-4 mt-2 text-neutral-500">
+                        <li>PreSonus Eris 3.5 Monitors</li>
+                        <li>Scarlett 4i4 Interface</li>
+                        <li>Mackie HM-4 Headphone Amplifier</li>
+                        <li>Shure SM7B Microphone</li>
+                        <li>Elgato Wave Mic Arm</li>
+                        <li>Logitech C920 Webcam</li>
+                        <li>Logic Pro X</li>
+                        <li>Final Cut Pro</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <span className="font-medium tracking-wide">Music &mdash;</span>
+                    <ul className="text-sm list-disc ml-4 mt-2 text-neutral-500">
+                        <li>Gibson Les Paul</li>
+                        <li>Orange Crush Pro 120 Amp Head + Orange FS2</li>
+                        <li>Peavey 5150 4 x 12 Cabinet</li>
+                        <li>Akai MPK Mini MKIII</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
