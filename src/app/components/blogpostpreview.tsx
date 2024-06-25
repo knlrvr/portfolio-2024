9d8d2a01015077f022424305503dcc608b9ae248
Kane <kanelariviere@outlook.com>
2024-06-25 14:39:34 -0400

    minor changes

--- src/app/components/blogpostpreview.tsx
+++ src/app/components/blogpostpreview.tsx
@@ -8,7 +8,7 @@ const BlogPostPreview = (props: PostMetadata) => {
     <Reveal>
       <Link key={props.slug} className="flex flex-col space-y-2 mb-12 group"
         href={`/blog/${props.slug}`}>
-        <p className="font-light tracking-wider group-hover:underline underline-offset-4 text-sm">{props.title}</p>
+        <p className="group-hover:underline underline-offset-4 font-medium tracking-wide text-sm">{props.title}</p>
         <p className="text-sm font-light text-neutral-400">{props.description}</p>
         <span className="text-xs tracking-wider text-neutral-500">{props.date}</span>
       </Link>
