af7867bb3cc04441b0361c3dc994550f7b4e8bd4
knlrvr <kanelariviere@outlook.com>
2024-11-07 22:18:50 -0500

    authorcard update

--- src/app/components/authorcard.tsx
+++ src/app/components/authorcard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Reveal } from '../utils/reveal'
+import { Braces, MapPin } from 'lucide-react'
 
 export default function AuthorCard() {
     return (
@@ -14,8 +15,14 @@ export default function AuthorCard() {
                 />
                 <div className="flex flex-col">
                     <p className="font-light text-2xl tracking-wide">Kane Lariviere</p>
-                    <p className="text-neutral-400 font-light tracking-wide text-sm">Software Developer</p>
-                    <p className="text-neutral-500 tracking-wide text-sm">@knlrvr</p>
+                    <div className="text-neutral-600 dark:text-neutral-400 tracking-wide text-sm flex items-center gap-1">
+                        <Braces className="w-3 h-3" />
+                        <p>Software Developer</p>
+                    </div>
+                    <div className="text-neutral-500 tracking-wide text-sm flex items-center gap-1">
+                        <MapPin className="w-3 h-3" />
+                        <p>Charlotte, NC, USA</p>
+                    </div>
                 </div>
             </div>
         </Reveal>
