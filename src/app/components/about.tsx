f721f65edfea19757695805ea86d4b37cef4e3eb
knlrvr <kanelariviere@outlook.com>
2024-11-11 15:52:35 -0500

    change

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -2,8 +2,8 @@ import { Section, Subsection } from "./section";
 
 export default function About() {
     return (
-        <Section title="Kane Lariviere">
-            <Subsection title="About">
+        <Section title="About">
+            <Subsection title="Kane Lariviere">
                 <p className="text-sm tracking-wide mb-16 text-neutral-600 dark:text-neutral-400">
                     <span className="text-[#111] dark:text-white">Software Developer based in the US.</span> Crafting engaging and memorable experiences. Exploring sustainability & advocating for a greener web. <em className="text-[#111] dark:text-white">Always shipping</em>.
                 </p>
