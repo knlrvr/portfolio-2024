db1ca2380d3e51e0604587488ec06c4daad83104
knlrvr <kanelariviere@outlook.com>
2025-01-11 15:30:58 -0500

    man

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -4,7 +4,7 @@ export default function About() {
     return (
             <Section title="About">
                 <p className="text-sm tracking-wide text-neutral-600 dark:text-neutral-400">
-                    Hi, I'm Kane! I'm a <span className="font-medium text-[#111] dark:text-white">software developer based in Charlotte, North Carolina.</span> Crafting engaging and memorable experiences. Exploring sustainability & advocating for a greener web. <em className="font-medium text-[#111] dark:text-white">Always shipping</em>.
+                    Hi, I&apos;m Kane! I&apos;m a <span className="font-medium text-[#111] dark:text-white">software developer based in Charlotte, North Carolina.</span> Crafting engaging and memorable experiences. Exploring sustainability & advocating for a greener web. <em className="font-medium text-[#111] dark:text-white">Always shipping</em>.
                 </p>
             </Section>
     )
